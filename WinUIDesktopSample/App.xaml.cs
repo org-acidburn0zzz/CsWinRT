86198747429d5ecaea7177e0abe7beb2a7763a3a
Scott Jones <sjones@microsoft.com>
2020-11-06 16:54:04 -0800

    simplified leak repro, using desktop app

--- WinUIDesktopSample/App.xaml.cs
+++ WinUIDesktopSample/App.xaml.cs
@@ -24,26 +24,12 @@ namespace WinUIDesktopSample
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             var value = DependencyProperty.UnsetValue;
-            var button = new Button
-            {
-                Content = "Click me to load MainPage",
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center
-            };
-            button.Click += Button_Click;
             var window = new Microsoft.UI.Xaml.Window
             {
-                Content = button
+                Content = new MainPage()
             };
-
             window.Activate();
-
             myWindow = window;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            myWindow.Content = new MainPage();
         }
     }
 
@@ -52,7 +38,6 @@ namespace WinUIDesktopSample
         static void Main(string[] args)
         {
             WinRT.ComWrappersSupport.InitializeComWrappers();
-
             Microsoft.UI.Xaml.Application.Start((e) => new App());
         }
     }
